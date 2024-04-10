dd32924cd480bb7ccb4bd894ee0da50f971a95d5
oaramos <oaramos@berkeley.edu>
2024-04-10 12:21:50 -0700

    changes to viewport, using marker camera

--- src/components/demo.js
+++ src/components/demo.js
@@ -1,10 +1,14 @@
+// import './demo.css';
 export default function Demo() {
   return (
-    <a-scene arjs="trackingMethod: best; sourceType:webcam; debugUIEnabled: false;">
+    <a-scene
+      xr-mode-ui="enabled: false;"
+      arjs="trackingMethod: best; sourceType:webcam; debugUIEnabled: false;"
+    >
       <a-marker preset="hiro">
         <a-box color="red"></a-box>
       </a-marker>
-      <a-entity camera></a-entity>
+      <a-marker-camera></a-marker-camera>
     </a-scene>
   );
 }
