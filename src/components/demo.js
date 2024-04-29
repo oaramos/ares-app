d3872020473f4a9f4177c3e3a6b7b291739b2a77
oaramos <oaramos@berkeley.edu>
2024-04-29 16:16:05 -0700

    Added custom berkeley logo pattern for marker, not permanent as we are waiting for changemake icon asset

--- src/components/demo.js
+++ src/components/demo.js
@@ -1,11 +1,15 @@
 // import './demo.css';
+
+var PATTERN_URL =
+  'https://raw.githubusercontent.com/oaramos/ares-app/main/src/assets/pattern-berkeley_icon.patt';
+
 export default function Demo() {
   return (
     <a-scene
       xr-mode-ui="enabled: false;"
       arjs="trackingMethod: best; sourceType:webcam; debugUIEnabled: false;"
     >
-      <a-marker preset="hiro">
+      <a-marker type="pattern" url={PATTERN_URL}>
         <a-box color="red"></a-box>
       </a-marker>
       <a-marker-camera></a-marker-camera>
